fae136fb5cef07c46f7b1df1fd9561b854d25cbb
Seal5 <121348218+Seal5@users.noreply.github.com>
2024-06-03 19:16:36 -0400

    Fix

--- frontend/src/Components/IngredientList/IngredientList.tsx
+++ frontend/src/Components/IngredientList/IngredientList.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Ingredient from "../Ingredient/Ingredient"
 
 type Props = {}
+let rating = 3
 
 const IngredientList = (props: Props) => {
   return (
     <div>
-      <Ingredient name="Red-40" description="Very Bad for You" rating={2}/>
-      <Ingredient name="Red-40" description="Very Bad for You" rating={0}/>
-      <Ingredient name="Red-40" description="Very Bad for You" rating={0}/>
+      <Ingredient name="Red-40" description="Very Bad for You" rating={rating}/>
+      <Ingredient name="Red-40" description="Very Bad for You" rating={rating}/>
+      <Ingredient name="Red-40" description="Very Bad for You" rating={rating}/>
     </div>
   )
 }
