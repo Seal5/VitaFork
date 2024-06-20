12167e7230601b6ed2c059574b8f2d1b199d40db
Seal5 <121348218+Seal5@users.noreply.github.com>
2024-06-20 15:14:54 -0400

    Backend Setup

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,24 +1,15 @@
 import axios from "axios"
 
-const apiKey = 'lsLVruYPiCZchptY2QbtrSSfuZC6QlepEB1fCNHF';
-
-export const searchIngredient = async (query: string) => {
-    const searchUrl = `https://api.nal.usda.gov/fdc/v1/foods/search?query=${query}&api_key=${apiKey}`;
+export const getIngredient = async (name: string) => {
+    const searchURL = 'https://world.openfoodfacts.org/cgi/search.pl?search_terms=${name}&search_simple=1&action=process&json=1'
     try {
-        const response = await axios.get(searchUrl);
-        const fdcId = response.data.foods[0].fdcId;
-        return fdcId;
+        const response = await axios.get(searchURL); 
+        if (response.data.length > 0) {
+            return response.data.products[0]
+        } else {
+            throw new Error('Ingredients not found')
+        }
     } catch (error) {
-        console.error('Error searching for ingredient:', error);
+        console.error('Error searching for ingredient', error)
     }
-};
-
-export const getIngredientDetails = async (fdcId: number) => {
-    const detailsUrl = `https://api.nal.usda.gov/fdc/v1/food/${fdcId}?api_key=${apiKey}`;
-    try {
-        const response = await axios.get(detailsUrl);
-        return response.data;
-    } catch (error) {
-        console.error('Error fetching ingredient details:', error);
-    }
-};
+}
